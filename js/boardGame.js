66a4f0a206dbe2a73ada72114fffebcef9275481
al7262 <alta5@alta5>
2019-12-26 17:42:20 +0700

    make boardGame

---
+++ js/boardGame.js
@@ -0,0 +1,87 @@
+(function () {
+    const canvas = document.getElementById('canvas'),
+        context = canvas.getContext('2d'),
+
+        tileSprite = new Image(),
+        bgSprite = new Image();
+    tileSprite.src = 'img/tile.jpg';
+    bgSprite.src = 'img/background.jpg';
+
+    function Tile(size, x, y){
+        this.size = size;
+        this.x = x*this.size;
+        this.y = y*this.size;
+        this.isHidden = true;
+
+        this.draw = ()=>{
+            var x = this.x, y = this.y;
+            if(this.isHidden){
+                context.drawImage(tileSprite, x, y);
+            } else {
+                context.drawImage(bgSprite, x, y)
+            }
+        }
+    };
+
+    function Board(width, height, tileSize){
+        this.width = width;
+        this.height = height;
+        this.tileSize = tileSize;
+        this.tiles = [];
+        this.numOfHiddenTiles = width*height;
+
+        this.init = ()=>{
+            let row, col;
+
+            for(row = this.height; row>0; row--){
+                this.tiles[row] = [];
+                for(col = this.width; col>0; col--){
+                    this.tiles[row][col] = new Tile(tileSize, row, col);
+                }
+            }
+        };
+
+        this.draw = ()=>{
+            var row, col;
+
+            for (row = this.height; row > 0; row--) {
+                for (col = this.width; col > 0; col--) {
+                    this.tiles[row][col].draw();
+                }
+            }
+        };
+
+        this.reveal = (x, y) => {
+            const clickedTile = that.tiles[x][y];
+            if(clickedTile.isHidden) {
+                clickedTile.isHidden = false;
+                this.numOfHiddenTiles--;
+            }
+            clickedTile.draw();
+        }
+    };
+
+    function Game(width, height){
+        this.width = width;
+        this.height = height;
+        this.tileSize = 32;
+        this.board = new Board(this.width, this.height, this.tileSize);
+
+        this.init = () => {
+            canvas.width = width * this.tileSize;
+            canvas.height = height * this.tileSize;
+            this.board.init();
+            this.board.draw();
+            // tileSprite.onload = () => {
+            //     this.board.draw()
+            // };
+        };
+    };
+
+    window.onload = () => {
+        console.log('start');
+        const game = new Game(20, 20);
+        game.init();
+        
+    }
+}());
