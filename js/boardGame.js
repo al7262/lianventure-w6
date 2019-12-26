d7473c89958d375e64cf7c21e21d219f4e916214
Willy Sumarno <willy@alterra.id>
2019-12-26 21:47:23 +0700

    edit board game

--- js/boardGame.js
+++ js/boardGame.js
@@ -3,8 +3,9 @@
         context = canvas.getContext('2d'),
 
         tileSprite = new Image(),
-        bgSprite = new Image();
+        narutoSprite = new Image();
     tileSprite.src = 'img/treeTile.png';
+    narutoSprite.src = 'img/narutoTile.png';
     bgSprite.src = 'img/grassTile.png';
 
     function Tile(size, x, y){
@@ -15,10 +16,16 @@
 
         this.draw = ()=>{
             var x = this.x, y = this.y;
+            
             if(this.isHidden){
                 context.drawImage(tileSprite, x, y);
             } else {
-                context.drawImage(bgSprite, x, y)
+            }
+
+            if(!this.hidden){
+                if(this.isNaruto) {
+                    context.drawImage(narutoSprite, x, y);
+                } 
             }
         }
     };
@@ -52,13 +59,30 @@
         };
 
         this.reveal = (x, y) => {
-            const clickedTile = that.tiles[x][y];
+            const clickedTile = this.tiles[x][y];
             if(clickedTile.isHidden) {
                 clickedTile.isHidden = false;
                 this.numOfHiddenTiles--;
             }
             clickedTile.draw();
+            this.draw();
         }
+
+        this.naruto = (x, y, prevX, prevY) => {
+            const currentTile = this.tiles[x][y];
+            const prevTile = this.tiles[prevX][prevY];
+            if(!currentTile.isNaruto){
+                currentTile.isNaruto = true;
+            }
+            if(prevTile.isNaruto){
+                prevTile.isNaruto = false;
+            }
+        }
+    };
+
+    function Naruto(x, y) {
+        this.x = x;
+        this.y = y;
     };
 
     function Game(width, height){
@@ -66,10 +90,54 @@
         this.height = height;
         this.tileSize = 32;
         this.board = new Board(this.width, this.height, this.tileSize);
+        this.naruto = new Naruto(5, 5);
+
+        // ===== MOVE HANDLER ===== //
+
+        this.move = (e) => {
+            this.board.naruto(5,5,4,4)
+            this.board.reveal(this.naruto.x, this.naruto.y);
+            // this.board.tiles[this.naruto.x][this.naruto.y+1].draw();
+            
+            if (e.keyCode == '38') {
+                this.naruto.y -= 1;
+                this.board.reveal(this.naruto.x, this.naruto.y);
+                this.board.naruto(this.naruto.x, this.naruto.y, this.naruto.x, this.naruto.y+1);
+                // this.board.tiles[this.naruto.x][this.naruto.y+1].draw();
+            }
+            else if (e.keyCode == '40') {
+                this.naruto.y += 1;
+                this.board.reveal(this.naruto.x, this.naruto.y);
+                this.board.naruto(this.naruto.x, this.naruto.y, this.naruto.x, this.naruto.y-1);
+                // this.board.tiles[this.naruto.x][this.naruto.y-1].draw();
+            }
+            else if (e.keyCode == '37') {
+                this.naruto.x -= 1;
+                this.board.reveal(this.naruto.x, this.naruto.y);
+                this.board.naruto(this.naruto.x, this.naruto.y, this.naruto.x+1, this.naruto.y);
+                // this.board.tiles[this.naruto.x+1][this.naruto.y].draw();
+            }
+            else if (e.keyCode == '39') {
+                this.naruto.x += 1;
+                this.board.reveal(this.naruto.x, this.naruto.y);
+                this.board.naruto(this.naruto.x, this.naruto.y, this.naruto.x-1, this.naruto.y);
+                // this.board.tiles[this.naruto.x-1][this.naruto.y].draw();
+            }
+            this.board.tiles[this.naruto.x][this.naruto.y].isHidden = false;
+            // this.board.tiles[this.naruto.x][this.naruto.y].draw();
+            this.board.draw();
+
+        };
+
+        // ===== INITIALIZE GAME ===== //
 
         this.init = () => {
             canvas.width = width * this.tileSize;
             canvas.height = height * this.tileSize;
+
+            // canvas.removeEventListener("mousedown", this.init);
+            window.addEventListener("keydown", this.move); // OPTIONS = FALSE ?
+
             this.board.init();
             this.board.draw();
             // tileSprite.onload = () => {
